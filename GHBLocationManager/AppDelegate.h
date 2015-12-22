5b67f4bed457ce6629ecf0c4a4d794fb28e9c8a7
123 <Hayben_gong@163.com>
2015-12-22 13:49:18 +0800

    Initial Commit

---
+++ GHBLocationManager/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GHBLocationManager
+//
+//  Created by 123 on 15/12/22.
+//  Copyright © 2015年 haibin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ GHBLocationManager/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GHBLocationManager
+//
+//  Created by 123 on 15/12/22.
+//  Copyright © 2015年 haibin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
